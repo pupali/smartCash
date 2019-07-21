c4d16985717bb1ebfd61fd4dca52181bc4b22463
Radwan1996 <radwan.adnanl1996@yahoo.com>
2019-07-21 21:21:25 +0300

    Add files via upload

---
+++ MProductCatagory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCasherProject.Models
+{
+    class MProductCatagory : INotifyPropertyChanged
+    {
+
+        private int _catagoryId;
+        public int catagoryId
+        {
+            get
+            { return _catagoryId; }
+            set
+            {
+                if (value == _catagoryId) return;
+                _catagoryId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _catagoryName;
+        public string catagoryName
+        {
+            get
+            { return _catagoryName; }
+            set
+            {
+                if (value == _catagoryName) return;
+                _catagoryName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _catagoryImage;
+        public string catagoryImage
+        {
+            get
+            { return _catagoryImage; }
+            set
+            {
+                if (value == _catagoryImage) return;
+                _catagoryImage = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        internal void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
