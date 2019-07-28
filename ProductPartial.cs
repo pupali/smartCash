7e71480a111df786dda501f7381fea4c67dc4c7c
Radwan1996 <radwan.adnanl1996@yahoo.com>
2019-07-28 10:41:04 +0300

    2 classes(InotifyPropertChanged) 12classes partial
    2 classes ---> Product , ProductCatagory (INotifyPropertyChnaged)
    12 classes ---> Partial

---
+++ ProductPartial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCasherProject.Entities.ProductManagmentEntities
+{
+    public partial class Product
+    {
+
+    }
+}
